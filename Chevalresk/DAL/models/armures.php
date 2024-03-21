bfdee29bf5634c4a3fc18597199c4acf926168cc
Mathieu007657 <mathieulavallee007657@gmail.com>
2024-03-21 13:17:55 -0400

    Create armures.php

---
+++ Chevalresk/DAL/models/armures.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once 'DAL/models/record.php';
+class Armure extends Record
+{
+    public $ItemId;
+    public $Taille;
+    public $Matiere;
+   
+    public function __construct($recordData = null)
+    {
+        $this->ItemId = 0;
+        $this->Taille = '';
+        $this->Matiere = '';
+        $this->setCompareKey('CreationDate');
+        parent::__construct($recordData);
+    }
+    public function setItemId($photoId)
+    {
+        $this->ItemId = (int) $photoId;
+    }
+    public function setTaille($taille)
+    {
+        $this->Taille = (int) $taille;
+    }
+}
