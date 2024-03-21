b16161e8f6e7a589c10b796b9af0b694b7f6711b
Mathieu007657 <mathieulavallee007657@gmail.com>
2024-03-21 13:01:21 -0400

    Create items.php
    a

---
+++ Chevalresk/DAL/models/items.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once 'DAL/models/record.php';
+class Item extends Record
+{
+    public $Id;
+    public $UserId; 
+    public $ItemId;
+   
+    public function __construct($recordData = null)
+    {
+        $this->Id = 0;
+        $this->UserId = 0;
+        $this->ItemId = 0;
+        $this->setCompareKey('CreationDate');
+        parent::__construct($recordData);
+    }
+    public function setUserId($userId)
+    {
+        $this->UserId = (int) $userId;
+    }
+    public function setItemId($photoId)
+    {
+        $this->ItemId = (int) $photoId;
+    }
+}
