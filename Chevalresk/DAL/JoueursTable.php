e72b4df3f08178195f078e36179aa2afa614b56b
Mathieu007657 <mathieulavallee007657@gmail.com>
2024-03-26 14:32:25 -0400

    Update JoueursTable.php

--- Chevalresk/DAL/JoueursTable.php
+++ Chevalresk/DAL/JoueursTable.php
@@ -23,6 +23,13 @@ final class JoueursTable extends MySQLTable
             return $user[0];
         return null;
     }
+    public function findByID($id)
+    {
+        $user = $this->selectWhere("idJoueurs = '$id'");
+        if (isset($user[0]))
+            return $user[0];
+        return null;
+    }
     public function userValid($email, $motdepasse)
     {
         $user = $this->selectWhere("courriel = '$email'");
