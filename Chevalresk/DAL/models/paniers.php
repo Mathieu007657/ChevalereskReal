c22376221fd00c7a0a6ff83b3ebc9ae14606e4ce
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-03-27 11:32:21 -0400

    fabricio

--- Chevalresk/DAL/models/paniers.php
+++ Chevalresk/DAL/models/paniers.php
@@ -3,31 +3,31 @@
 include_once 'DAL/models/record.php';
 class Panier extends Record
 {
-    public $ItemId;
-    public $UserId;
+    public $idItem;
+    public $idJoueurs;
     public $QuantiteAchat;
    
     public function __construct($recordData = null){
-        $this->ItemId = 0;
-        $this->UserId = 0;
+        $this->idItem = 0;
+        $this->idJoueurs = 0;
         $this->QuantiteAchat = 0;
         parent::__construct($recordData);
     }
     //SET Item ID
     public function setItemId($itemId){
-        $this->ItemId = (int) $itemId;
+        $this->idItem = (int) $itemId;
     }
     //GET Item ID
     public function getItemId(){
-        return $this->ItemId;
+        return $this->idItem;
     }
     //SET User ID
-    public function setUserId($userId){
-        $this->UserId = (int) $userId;
+    public function setidJoueurs($userId){
+        $this->idJoueurs = (int) $userId;
     }
     //GET User ID
-    public function getUserId(){
-        return $this->UserId;
+    public function getidJoueurs(){
+        return $this->idJoueurs;
     }
     //SET Quantité Achat
     public function setQuantiteAchat($quantiteAchat){
--- Chevalresk/Paniers.php
+++ Chevalresk/Paniers.php
@@ -9,18 +9,36 @@ userAccess();
 $viewTitle = "Panier";
 $viewContent = "<div class='photosLayout'>";
 $isAdmin = (bool) $_SESSION["isAdmin"];
-$idPlayer = $_SESSION['currentUserId'];
+$idPlayer = $_SESSION['currentUserId'] +1;
 $list = PanierTable()->get($idPlayer);
 
 foreach ($list as $item) {
     $idItemPan = $item->idItem;
     $quantity = $item->quantiteAchat;
     $ItemSelect = ItemTable()->get($idItemPan);
-
+    $id = $ItemSelect->idItem;
     $nom = $ItemSelect->nom;
     $prix = $ItemSelect->prix;
     $photo = $ItemSelect->photo;
-    
+    $lienEcu="images/ecu.png";
+    $lienPhoto="data/images/photoItem/"."$photo";
+
+    $ItemPanier = <<<HTML
+                        <div class="photoLayout" photo_id="$id">
+                    <div class="photoTitleContainer">
+                        <div class="photoTitle ellipsis">$name</div>
+                    </div>
+                    <a href="addItemPanier.php?id=$id">
+                        <div class="photoImage" style="background-image:url('$lienPhoto')"></div>
+                        <div>
+                            <div>
+                                Prix: $prix écus <img src="$lienEcu" class="appLogo">
+                            </div>
+                        </div>
+                    </a>
+                </div>
+    HTML;
+    $viewContent = $viewContent . $photoHTML;
     
 }
 $viewContent = $viewContent . "</div>";
