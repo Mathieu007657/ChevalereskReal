3604e9ce16df8acececdff22944b380b48147be0
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-04-02 13:25:01 -0400

    Update header.php

--- Chevalresk/views/header.php
+++ Chevalresk/views/header.php
@@ -44,12 +44,7 @@ if (isset($_SESSION["validUser"])) {
         </a>
     HTML;
     $connectedUserAvatar = <<<HTML
-<<<<<<< HEAD
-        <div class="UserAvatarSmall" style="background-image:url('/data/avatars/$avatar')" title="$userName"></div>
-        <div>$solde<img src="images/ecu.png" class="imgEcu"></div>
-=======
         <div class="UserAvatarSmall" style="background-image:url('data/images/avatars/$avatar')" title="$userName"></div>
->>>>>>> 43e1a8dfa2b394ee982e2138a384217320417557
     HTML;
 } else {
     $loggedUserMenu = <<<HTML
