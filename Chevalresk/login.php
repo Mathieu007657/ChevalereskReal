1dbd97d8ba2a5ed4537235b9e1e51ac09d8ea1f0
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-04-02 23:14:00 -0400

    Update login.php

--- Chevalresk/login.php
+++ Chevalresk/login.php
@@ -44,7 +44,7 @@ if (isset($_POST['submit'])) {
         
         $_SESSION['validUser'] = true;
         $_SESSION['isAdmin'] = $User->isAdmin();
-        $_SESSION['currentUserId'] = $User->JoueurId+1;
+        $_SESSION['currentUserId'] = $User->JoueurId;
         //$_SESSION['currentUserId']=$_SESSION['currentUserId']+1;
         $_SESSION['userName'] = $User->Name;
         $_SESSION['avatar'] = $User->Avatar;
