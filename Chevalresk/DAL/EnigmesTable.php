9d89b140d4b092aae1cd759a67437d3e73119ca9
MrWelz <thierrybecker28@gmail.com>
2024-04-17 10:08:34 -0400

    Update EnigmesTable.php

--- Chevalresk/DAL/EnigmesTable.php
+++ Chevalresk/DAL/EnigmesTable.php
@@ -5,18 +5,20 @@ include_once 'php/imageFiles.php';
 
 final class EnigmesTable extends MySQLTable
 {
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct(DB(), new Enigme());
     }
 
     //À faire
-    public function getEnigme() {
+    public function getEnigme($difficulte)
+    {
         $tableName = "Enigmes";
-    
+
         // Obtenir le nombre d'énigmes non pigées
         $countQuery = "SELECT count(Enonce) FROM dbchevalersk8.$tableName WHERE estPigee = 'N';";
         $countResult = $this->_DB->querySqlCmd($countQuery);
-    
+
         // Vérifier si le résultat est un tableau
         if (is_array($countResult)) {
             $countResult = $this->toObjectArray($countResult);
@@ -25,23 +27,29 @@ final class EnigmesTable extends MySQLTable
             // Gérer le cas où $countResult n'est pas un tableau
             $numUnpicked = 0;
         }
-    
+
         // Générer un ID aléatoire pour sélectionner une énigme non pigée
         $idRandom = rand(0, $numUnpicked + 1);
 
         // Requête pour obtenir une énigme non pigée aléatoire
-        $sql = "SELECT Enonce FROM dbchevalersk8.$tableName WHERE estPigee = 'N' AND idEnigme = $idRandom;";
-        $data = $this->_DB->querySqlCmd($sql);
-
+        if ($difficulte === "") {
+            $sql = "SELECT Enonce FROM dbchevalersk8.$tableName WHERE estPigee = 'N' AND idEnigme = $idRandom;";
+            $data = $this->_DB->querySqlCmd($sql);
+        }
+        else{
+            $sql = "SELECT Enonce From dbchevalersk8.$tableName WHERE estPigee = 'N' AND idEnigme = $idRandom AND Difficile = $difficulte;";
+            $data = $this->_DB->querySqlCmd($sql);
+        }
         return $data;
-    }    
-    public function getDifficulte($question) {
+    }
+    public function getDifficulte($question)
+    {
         $tableName = "Enigmes";
 
         // Requête pour obtenir une énigme non pigée aléatoire
         $sql = "SELECT Enonce, Difficulte FROM dbchevalersk8.$tableName WHERE estPigee = 'N' AND idEnigme = $question;";
         $data = $this->_DB->querySqlCmd($sql);
-    
+
         // Vérifier si des données ont été récupérées
         if ($data && count($data) > 0) {
             // Récupérer l'énoncé de l'énigme depuis le tableau retourné
@@ -53,22 +61,23 @@ final class EnigmesTable extends MySQLTable
             } elseif ($data[0]['Difficulte'] == 'D') {
                 $difficulte = "Difficile";
             }
-            $enigme = $data[0]['Enonce'] . " (".$difficulte.")";
+            $enigme = $data[0]['Enonce'] . " (" . $difficulte . ")";
             return $enigme;
         } else {
             // Aucune énigme trouvée, retourner null ou gérer l'erreur selon vos besoins
             return null;
         }
     }
-    public function getReponses($question) {
+    public function getReponses($question)
+    {
         $tableName = "Enigmes";
-        
+
         $sql1 = "SELECT idEnigme FROM dbchevalersk8.$tableName WHERE Enonce Like '%$question%';";
         $data1 = $this->_DB->querySqlCmd($sql1);
         $tableName = "Reponses";
         $sql = "SELECT LaReponse FROM dbchevalersk8.$tableName WHERE idEnigme = $data1;";
         $data = $this->_DB->querySqlCmd($sql);
-    
+
         // Vérifier si des données ont été récupérées
         if ($data && count($data) > 0) {
             //ici, dis toi qu'il y a 4 reponses en tout avec estBonne, pas 5 et faire un foreach, puisqu'il n'y a pas de Choix1,2,3,4
@@ -77,7 +86,7 @@ final class EnigmesTable extends MySQLTable
             $choix3 = $data[0]['Choix3'];
             $choix4 = $data[0]['Choix4'];
             $estBonne = $data[0]['EstBonne'];
-    
+
             // Générer le contenu HTML avec les choix et le bouton de vérification
             $reponsesContent = <<<HTML
                 <div class='Reponses'>
@@ -94,17 +103,18 @@ final class EnigmesTable extends MySQLTable
                     </form> 
                 </div>
     HTML;
-    
+
             return $reponsesContent;
         } else {
             // Aucune donnée trouvée, retourner une chaîne vide ou gérer l'erreur selon vos besoins
             return "";
         }
     }
-    
-    
+
+
     //À faire
-    public function updateEnigme($data){
+    public function updateEnigme($data)
+    {
         $sql = "UPDATE  SET  = $data->";
         return $this->_DB->nonQuerySqlCmd($sql);
     }
