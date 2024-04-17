aa916573a472665b336886bde0ec57fb6ddd825d
PhilippeRobichaudGionet <prg@hotmail.ca>
2024-04-17 11:33:33 -0400

    saluet

--- Chevalresk/Panoramix.php
+++ Chevalresk/Panoramix.php
@@ -4,26 +4,33 @@ include 'php/sessionManager.php';
 include 'php/formUtilities.php';
 include 'php/date.php';
 require 'DAL/ChevalereskDB.php';
-
+$viewTitle = "Panoramix";
 $ListPotionitem = ItemTable()->selectWhere("type = 'P'");
+
 foreach ($ListPotionitem as $Potion) {
     $idPotion = $Potion->IdItem;
     $NamePotion = $Potion->Nom;
-    $lienPhoto = $Potion->Photo;
-    echo "<div class='ZonePotion'>";
-    $ZoneHTML = <<<HTML
-        <div class="photoImage" style="background-image:url('$lienPhoto')"></div>
-        <h2>$NamePotion</h2>
-        <i class="fa-solid fa-arrow-right-long"></i>
+    $lienPhoto = $lienPhoto="data/images/photoItem/"."$Potion->Photo";
+    $viewContent =  "<div class='ZonePotion'>";
+    $viewContent .= <<<HTML
+        <div class="ZoneOnePotion">
+            <div class="PotionRow">
+                <div class="PanoramixPotionImage" style="background-image:url('$lienPhoto')" title="$NamePotion"></div>
+                <h2 class="NamePotion">$NamePotion</h2>
+            </div>
+            <div>
+                <i class="fa-solid fa-chevron-right arrow"></i>
+            </div>
+        </div>
     HTML;
     $ElementDePotion = ConcocterTable()->selectWhere("Potions_idItem = $idPotion");
-    echo "<div>";
+    $viewContent .= "<div class='ElementRow'>";
     foreach ($ElementDePotion as $elem) {
         $photoElem = $elem->Photo;
-        $ZoneHTML .= <<<HTML
+        $viewContent .= <<<HTML
         <div class="photoImage" style="background-image:url('$photoElem')"></div>
     HTML;
     }
-        echo "<div>";
-    echo "</div>";
+    $viewContent .= "</div></div>";
 }
+include "views/master.php";
