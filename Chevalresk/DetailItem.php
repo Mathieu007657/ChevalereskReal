7a665c503154ff254de2a8a0dc4cc6d8d37928ad
Mathieu007657 <mathieulavallee007657@gmail.com>
2024-05-14 13:36:54 -0400

    commit

--- Chevalresk/DetailItem.php
+++ Chevalresk/DetailItem.php
@@ -11,40 +11,29 @@ $type = ItemTable()->findType($id);
 $infoPrix = "Prix: $prix";
 $lienEcu = "images/ecu.png";
 $lienPhoto = "data/images/photoItem/" . "$photo";
+$lienEtoile = "images/Etoile.webp";
+$lienEtoileHalf = "images/EtoileHalf.png";
 $estAlch = ItemTable()->findAlch($_SESSION['currentUserId']);
 $ajout = '<a href="addItemPanier.php?id=$id"><button class="button-34" role="button">Ajouter au panier</button></a>';
 
 
 
-if($type == 'Armes')
-{
-    $query = ItemTable()->findInfosArmes($type,$id);
-}
-else if($type == 'Armures')
-{
-    $query = ItemTable()->findInfosArmures($type,$id);
-}
-else if($type == 'Potions')
-{
-    $query = ItemTable()->findInfosPotions($type,$id);
-    if($estAlch == 1)
-    {
-        $ajout = '<a href="addItemPanier.php?id=$id"><button class="button-34" role="button">Ajouter au panier</button></a>';  
-    }
-    else
-    {
+if ($type == 'Armes') {
+    $query = ItemTable()->findInfosArmes($type, $id);
+} else if ($type == 'Armures') {
+    $query = ItemTable()->findInfosArmures($type, $id);
+} else if ($type == 'Potions') {
+    $query = ItemTable()->findInfosPotions($type, $id);
+    if ($estAlch == 1) {
+        $ajout = '<a href="addItemPanier.php?id=$id"><button class="button-34" role="button">Ajouter au panier</button></a>';
+    } else {
         $ajout = '<br> <br> Vous ne pouvez acheter cet item tant que vous n\'êtes pas alchimiste';
     }
-}
-else if($type == 'Elements')
-{
-    $query = ItemTable()->findInfosElements($type,$id);
-    if($estAlch == 1)
-    {
-        $ajout = '<a href="addItemPanier.php?id=$id"><button class="button-34" role="button">Ajouter au panier</button></a>';  
-    }
-    else
-    {
+} else if ($type == 'Elements') {
+    $query = ItemTable()->findInfosElements($type, $id);
+    if ($estAlch == 1) {
+        $ajout = '<a href="addItemPanier.php?id=$id"><button class="button-34" role="button">Ajouter au panier</button></a>';
+    } else {
         $ajout = '<br> <br> Vous ne pouvez acheter cet item tant que vous n\'êtes pas alchimiste';
     }
 }
@@ -87,7 +76,15 @@ $html = <<<HTML
         background-position: 0% 50%;
     }
 }
-
+.etoileContainer  {
+  background-color: #000000;
+  border-radius: 999px;
+  height: 57.75px;
+  width: 40%;
+  margin: 0 auto;;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
 /* CSS */
 .button-34 {
   background: #5E5DF0;
@@ -110,6 +107,39 @@ $html = <<<HTML
   word-break: break-word;
   border: 0;
 }
+/* Three column layout */
+.side {
+  float: left;
+  width: 15%;
+  margin-top:10px;
+}
+
+.middle {
+  margin-top:20px;
+  float: left;
+  width: 70%;
+}
+
+/* Place text to the right */
+.right {
+  text-align: right;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* The bar container */
+.bar-container {
+  width: 100%;
+  background-color: #f1f1f1;
+  text-align: center;
+  color: white;
+}
+.bar {width: 4%; height: 18px; background-color: #ff9800; padding}
 </style>
 HTML;
 
@@ -130,12 +160,82 @@ $html .= <<<HTML
         </div>
         <br>
         <div>
+        <div style="margin: 0 auto; padding: 20px;">
+
+<div class="etoileContainer">
+    <img src="$lienEtoile" style="height:57.75px"/>
+    <img src="$lienEtoile" style="height:57.75px"/>
+    <img src="$lienEtoile" style="height:57.75px"/>
+    <img src="$lienEtoile" style="height:57.75px"/>
+    <img src="$lienEtoileHalf" style="width:30px"/>
+</div>
+<p>4.1/5 basé sur 254 évaluations.</p>
+<hr style="border:3px solid #f1f1f1">
+
+<div class="row">
+  <div class="side">
+    <div>5 étoiles</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar" style="width: 15%"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div>150%</div>
+  </div>
+  <div class="side">
+    <div>4 étoiles</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar"style="width: 15%"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div>63%</div>
+  </div>
+  <div class="side">
+    <div>3 étoiles</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar"style="width: 15%"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div>15%</div>
+  </div>
+  <div class="side">
+    <div>2 étoiles</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar"style="width: 15%"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div>6%</div>
+  </div>
+  <div class="side">
+    <div>1 étoile</div>
+  </div>
+  <div class="middle">
+    <div class="bar-container">
+      <div class="bar"style="width: 15%"></div>
+    </div>
+  </div>
+  <div class="side right">
+    <div>20%</div>
+  </div>
+</div>
+            <br>
+            <br>
             <a href="itemsList.php"><button class="button-34" role="button">Retour à la liste</button></a>
             <span style="color: red;">$ajout</span>
         </div>
     </div>
 </div>
 HTML;
-
 echo $html;
 ?>
